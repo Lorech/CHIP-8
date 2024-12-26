92c2516d378a68ba65fc636306a0f01fcedf7700
Lorech <laurisdedumets@gmail.com>
2024-12-26 18:15:23 +0200

    chore: Add Raylib boilerplate

--- src/main.c
+++ src/main.c
@@ -1,6 +1,20 @@
-#include <stdio.h>
+#include "raylib.h"
 
 int main() {
-    printf("Hello, World!\n");
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+
+    InitWindow(screenWidth, screenHeight, "Hello, World!");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText("Hello, World!", 10, 10, 20, DARKGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
     return 0;
 }

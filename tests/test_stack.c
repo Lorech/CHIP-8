8b214c0c2a3df754c183af3f66984ddb0fe89d08
Lorech <laurisdedumets@gmail.com>
2024-12-29 20:18:51 +0200

    feat: Add peek operation to stack

--- tests/test_stack.c
+++ tests/test_stack.c
@@ -37,6 +37,28 @@ void test_push_pop_works_as_stack()
     }
 }
 
+void test_peek_validates_underflow()
+{
+    uint16_t address;
+    TEST_ASSERT_FALSE(peek(&s, &address));
+}
+
+void test_push_peek_works_as_stack()
+{
+    uint16_t data[3] = {0x002, 0x020, 0x200};
+
+    for (uint8_t i = 0; i < sizeof(data) / sizeof(uint16_t); i++) {
+        TEST_ASSERT_TRUE(push(&s, data[i]));
+    }
+
+    for (uint8_t i = len(data) - 1; i < len(data) && i >= 0; i--) {
+        uint16_t address;
+        TEST_ASSERT_TRUE(peek(&s, &address));
+        // Keep the expected index static, as peek should not mutate the stack.
+        TEST_ASSERT_EQUAL_INT16(data[2], address);
+    }
+}
+
 void test_push_validates_overflow()
 {
     for (uint8_t i = 0; i < STACK_SIZE; i++) {
@@ -51,6 +73,8 @@ int main()
     UNITY_BEGIN();
     RUN_TEST(test_pop_validates_underflow);
     RUN_TEST(test_push_pop_works_as_stack);
+    RUN_TEST(test_peek_validates_underflow);
+    RUN_TEST(test_push_peek_works_as_stack);
     RUN_TEST(test_push_validates_overflow);
     return UNITY_END();
 }

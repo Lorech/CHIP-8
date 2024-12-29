7c4735892ce3df1f51376f54b208372125f3d8e5
Lorech <laurisdedumets@gmail.com>
2024-12-29 16:27:14 +0200

    chore: Unit test stack module

---
+++ tests/test_stack.c
@@ -0,0 +1,56 @@
+#include <stdint.h>
+
+#include "macros.h"
+#include "stack.h"
+#include "unity.h"
+
+static stack s;
+
+void setUp()
+{
+    init_stack(&s);
+}
+
+void tearDown()
+{
+    return;
+}
+
+void test_pop_validates_underflow()
+{
+    uint16_t address;
+    TEST_ASSERT_FALSE(pop(&s, &address));
+}
+
+void test_push_pop_works_as_stack()
+{
+    uint16_t data[3] = {0x002, 0x020, 0x200};
+
+    for (uint8_t i = 0; i < sizeof(data) / sizeof(uint16_t); i++) {
+        TEST_ASSERT_TRUE(push(&s, data[i]));
+    }
+
+    for (uint8_t i = len(data) - 1; i < len(data) && i >= 0; i--) {
+        uint16_t address;
+        TEST_ASSERT_TRUE(pop(&s, &address));
+        TEST_ASSERT_EQUAL_INT16(data[i], address);
+    }
+}
+
+void test_push_validates_overflow()
+{
+    for (uint8_t i = 0; i < STACK_SIZE; i++) {
+        TEST_ASSERT_TRUE(push(&s, 0x200));
+    }
+
+    TEST_ASSERT_FALSE(push(&s, 0x200));
+}
+
+int main()
+{
+    UNITY_BEGIN();
+    RUN_TEST(test_pop_validates_underflow);
+    RUN_TEST(test_push_pop_works_as_stack);
+    RUN_TEST(test_push_validates_overflow);
+    return UNITY_END();
+}
